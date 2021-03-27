61c5ad30d6248b3e4c829f4d6123b4cb5f018a69
Eugene Efimochkin <e.efimochkin@gmail.com>
2021-03-27 22:23:51 +0100

    Add "Reset" action

--- src/App.svelte
+++ src/App.svelte
@@ -12,9 +12,14 @@
 		let {operation, value, ...rest} = event.detail;
 		result = calc(operation, result, value);
 	}
+
+	function reset(event) {
+		// TODO former result must go into roster of operations
+		result = 0;
+	}
 </script>
 
 
 <OpsList />
 <Display {result}/>
-<Controls validOperations={Object.keys(operations)} on:commit={step} bind:operation={next_operation} bind:value={next_operand}/> = {calc(next_operation, result, next_operand)}
+<Controls validOperations={Object.keys(operations)} on:commit={step} on:reset={reset} bind:operation={next_operation} bind:value={next_operand}/> = {calc(next_operation, result, next_operand)}
--- src/Controls.svelte
+++ src/Controls.svelte
@@ -5,23 +5,42 @@
 	export let value = 0;
 	export let validOperations;
 
+	let armedForReset = false;
+
 	const flush = createEventDispatcher();
 
 	const validValuesRegexp = /0-9/;
 
-	function catchOperationChange(event){
-		operation = validOperations.find(el => el == event.key) || operation;
-		if(event.key !== '.') {
-			value = parseFloat(value) || 0;
+	function catchOperationChange(event) {
+		if (event.key == "Enter") {
+
+			if (armedForReset) {
+				flush('reset', {});
+				armedForReset = false;
+			} else {
+				flush("commit", { operation, value });
+				value = 0;
+				// TODO: "Clear" command as "double Enter"
+				armedForReset = true;
+			}
+		} else {
+			operation = validOperations.find((el) => el == event.key) || operation;
+			if (event.key !== ".") {
+				value = parseFloat(value) || 0;
+				armedForReset = false;
+			}
+			// TODO handle up arrow
+			// TODO handle down arrow
 		}
-		// TODO handle up arrow
-		// TODO handle down arrow
-		console.log(operation, value)
-		if (event.key == 'Enter')  {
-			flush('commit', {operation, value});
-			value = 0;
-		}
+
 	}
 </script>
+
+<p>
+	{#if armedForReset}
+		<small>Hit Enter to start new sequence</small>
+	{/if}
+	&nbsp;
+</p>
 
 {operation}<input type="number" bind:value={value} min=0 on:keyup={catchOperationChange}>
